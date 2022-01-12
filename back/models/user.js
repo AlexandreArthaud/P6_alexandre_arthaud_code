4abbdc9ceef52823ec1c06c6ab32008ee3829d6c
Alexandre Arthaud <alexandre.arthaud426@gmail.com>
2022-01-12 18:31:19 +0100

    add mongoose validator

--- back/models/user.js
+++ back/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
 const uniqueValidator = require('mongoose-unique-validator');
+
+const passwordValidator = [
+	validate({
+		validator: 'isLength',
+		arguments: [3, 40],
+		message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+	}),
+]
 
 const userSchema = mongoose.Schema({
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+	password: { type: String, required: true, validate: passwordValidator }
 });
 
 userSchema.plugin(uniqueValidator);
