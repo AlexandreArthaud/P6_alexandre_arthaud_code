ebe5d964772803d5bbcea63d0b4f6889325d3314
Alexandre Arthaud <alexandre@arthaud.eu>
2022-01-18 17:38:16 +0100

    use cookie-session module for more secure cookies

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const helmet = require('helmet');
+const session = require('cookie-session');
 
 require('dotenv').config()
 
@@ -21,13 +22,28 @@ mongoose.connect(
 .catch(() => console.log("Not connected to MongoDB"));
 
 app.use(helmet());
+
+// allow CORS
 app.use((req, res, next) => {
-	// allow CORS
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 	next();
 });
+
+// more secure cookies
+let expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+app.use(session({
+  name: 'session',
+	keys: ['key1', ' key2'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    domain: 'http://localhost:3000',
+    expires: expiryDate
+  }
+}));
+
 app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
